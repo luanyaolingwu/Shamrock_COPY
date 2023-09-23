48e61b0b7e3f494d63216f668468fb3c19144d3a
owo233 <callng@qq.com>
2023-09-24 05:06:24 +0800

    `Shamrock`: 更友好的日志输出API

--- xposed/src/main/java/moe/fuqiuluo/remote/api/LogExplorer.kt
+++ xposed/src/main/java/moe/fuqiuluo/remote/api/LogExplorer.kt
@@ -1,14 +1,17 @@
 package moe.fuqiuluo.remote.api
 
 import io.ktor.server.application.call
-import io.ktor.server.response.respondFile
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.Routing
 import moe.fuqiuluo.xposed.helper.LogCenter
 import moe.fuqiuluo.xposed.tools.getOrPost
+import moe.fuqiuluo.xposed.tools.fetchOrNull
 
 fun Routing.showLog() {
     getOrPost("/log") {
-        val log = LogCenter.getAllLog()
-        call.respondFile(log)
+        val start = fetchOrNull("start")?.toIntOrNull() ?: 0
+        val recent =fetchOrNull("recent")?.toBooleanStrictOrNull() ?: false
+        val log = LogCenter.getLogLines(start, recent)
+        call.respondText(log.joinToString("\n"))
     }
 }
