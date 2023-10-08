1f76e401ab01047cdd220625dda835d7a891730a
FQL <ydic001@vip.qq.com>
2023-10-08 14:20:42 +0800

    `Shamrock`: fix #50
    Signed-off-by: FQL <ydic001@vip.qq.com>

--- xposed/src/main/java/moe/fuqiuluo/remote/config/StatusPages.kt
+++ xposed/src/main/java/moe/fuqiuluo/remote/config/StatusPages.kt
@@ -8,6 +8,7 @@ import io.ktor.server.application.install
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.request.uri
 import io.ktor.server.response.respond
+import io.ktor.util.AttributeKey
 import moe.fuqiuluo.remote.entries.CommonResult
 import moe.fuqiuluo.remote.entries.ErrorCatch
 import moe.fuqiuluo.remote.entries.Status
@@ -15,16 +16,40 @@ import moe.fuqiuluo.remote.entries.Status
 fun Application.statusPages() {
     install(StatusPages) {
         exception<ParamsException> { call, cause ->
-            call.respond(CommonResult("failed", Status.BadParam.code, ErrorCatch(call.request.uri, cause.message ?: "")))
+            val echo = call.attributes[AttributeKey("echo")] as? String ?: ""
+            call.respond(CommonResult(
+                status = "failed",
+                retcode = Status.BadParam.code,
+                data = ErrorCatch(call.request.uri, cause.message ?: ""),
+                echo = echo
+            ))
         }
         exception<LogicException> { call, cause ->
-            call.respond(CommonResult("failed", Status.LogicError.code, ErrorCatch(call.request.uri, cause.message ?: "")))
+            val echo = call.attributes[AttributeKey("echo")] as? String ?: ""
+            call.respond(CommonResult(
+                status = "failed",
+                retcode = Status.LogicError.code,
+                data = ErrorCatch(call.request.uri, cause.message ?: ""),
+                echo = echo
+            ))
         }
         exception<ErrorTokenException> { call, cause ->
-            call.respond(CommonResult("failed", Status.ErrorToken.code, ErrorCatch(call.request.uri, cause.message ?: "")))
+            val echo = call.attributes[AttributeKey("echo")] as? String ?: ""
+            call.respond(CommonResult(
+                status = "failed",
+                retcode = Status.ErrorToken.code,
+                data = ErrorCatch(call.request.uri, cause.message ?: ""),
+                echo = echo
+            ))
         }
         exception<Throwable> { call, cause ->
-            call.respond(CommonResult("failed", Status.InternalHandlerError.code, ErrorCatch(call.request.uri, cause.stackTraceToString())))
+            val echo = call.attributes[AttributeKey("echo")] as? String ?: ""
+            call.respond(CommonResult(
+                status = "failed",
+                retcode = Status.InternalHandlerError.code,
+                data = ErrorCatch(call.request.uri, cause.message ?: ""),
+                echo = echo
+            ))
         }
     }
 }
