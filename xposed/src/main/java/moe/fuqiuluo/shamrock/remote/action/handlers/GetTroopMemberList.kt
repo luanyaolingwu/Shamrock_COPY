bb23b700dbeaf19747a265f9a8aa54ced86b958e
真心 <qgzhenxin@qq.com>
2023-10-18 18:47:01 +0800

    fix: 群成员信息未包含 `card` 字段 (close #122)

--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/action/handlers/GetTroopMemberList.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/action/handlers/GetTroopMemberList.kt
@@ -10,51 +10,58 @@ import moe.fuqiuluo.shamrock.remote.service.data.push.MemberRole
 import moe.fuqiuluo.shamrock.tools.EmptyJsonString
 import moe.fuqiuluo.shamrock.tools.ifNullOrEmpty
 
-internal object GetTroopMemberList: IActionHandler() {
+internal object GetTroopMemberList : IActionHandler() {
     override suspend fun internalHandle(session: ActionSession): String {
         val groupId = session.getString("group_id")
         val refresh = session.getBooleanOrDefault("refresh", false)
         return invoke(groupId, refresh, session.echo)
     }
 
-    suspend operator fun invoke(groupId: String, refresh: Boolean, echo: JsonElement = EmptyJsonString): String {
+    suspend operator fun invoke(
+        groupId: String,
+        refresh: Boolean,
+        echo: JsonElement = EmptyJsonString
+    ): String {
         val memberList = GroupSvc.getGroupMemberList(groupId, refresh).onFailure {
             return error(it.message ?: "unknown error", echo)
         }.getOrThrow()
 
         return ok(arrayListOf<SimpleTroopMemberInfo>().apply {
-            memberList.forEach {  info ->
+            memberList.forEach { info ->
                 if (info.memberuin != "0") {
                     add(
                         SimpleTroopMemberInfo(
-                        uin = info.memberuin.toLong(),
-                        name = info.friendnick.ifNullOrEmpty(info.autoremark) ?: "",
-                        showName = info.troopnick.ifNullOrEmpty(info.troopColorNick),
-                        distance = info.distance,
-                        honor = GroupSvc.parseHonor(info.honorList),
-                        joinTime = info.join_time,
-                        lastActiveTime = info.last_active_time,
-                        uniqueName = info.mUniqueTitle,
-                        groupId = groupId.toLong(),
-                        nick = info.friendnick.ifNullOrEmpty(info.autoremark) ?: "",
-                        sex = when(info.sex.toShort()) {
-                            Card.FEMALE -> "female"
-                            Card.MALE -> "male"
-                            else -> "unknown"
-                        },
-                        area = info.alias ?: "",
-                        lastSentTime = info.last_active_time,
-                        level = info.level,
-                        role = when {
-                            GroupSvc.getOwner(groupId).toString() == info.memberuin -> MemberRole.Owner
-                            info.memberuin.toLong() in GroupSvc.getAdminList(groupId) -> MemberRole.Admin
-                            else -> MemberRole.Member
-                        },
-                        unfriendly = false,
-                        title = info.mUniqueTitle ?: "",
-                        titleExpireTime = info.mUniqueTitleExpire,
-                        cardChangeable = GroupSvc.isAdmin(groupId)
-                    )
+                            uin = info.memberuin.toLong(),
+                            name = info.friendnick.ifNullOrEmpty(info.autoremark) ?: "",
+                            showName = info.troopnick.ifNullOrEmpty(info.troopColorNick),
+                            cardName = info.troopnick.ifNullOrEmpty(info.troopColorNick),
+                            distance = info.distance,
+                            honor = GroupSvc.parseHonor(info.honorList),
+                            joinTime = info.join_time,
+                            lastActiveTime = info.last_active_time,
+                            uniqueName = info.mUniqueTitle,
+                            groupId = groupId.toLong(),
+                            nick = info.friendnick.ifNullOrEmpty(info.autoremark) ?: "",
+                            sex = when (info.sex.toShort()) {
+                                Card.FEMALE -> "female"
+                                Card.MALE -> "male"
+                                else -> "unknown"
+                            },
+                            area = info.alias ?: "",
+                            lastSentTime = info.last_active_time,
+                            level = info.level,
+                            role = when {
+                                GroupSvc.getOwner(groupId)
+                                    .toString() == info.memberuin -> MemberRole.Owner
+
+                                info.memberuin.toLong() in GroupSvc.getAdminList(groupId) -> MemberRole.Admin
+                                else -> MemberRole.Member
+                            },
+                            unfriendly = false,
+                            title = info.mUniqueTitle ?: "",
+                            titleExpireTime = info.mUniqueTitleExpire,
+                            cardChangeable = GroupSvc.isAdmin(groupId)
+                        )
                     )
                 }
             }
