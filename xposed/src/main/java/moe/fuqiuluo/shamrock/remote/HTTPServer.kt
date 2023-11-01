fe7d9969711374863ab9513004f3abb527aa3bd5
luanyaolingwu <user3678169@163.com>
2023-11-02 00:12:30 +0800

    SYNC

--- xposed/src/main/java/moe/fuqiuluo/shamrock/remote/HTTPServer.kt
+++ xposed/src/main/java/moe/fuqiuluo/shamrock/remote/HTTPServer.kt
@@ -10,6 +10,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.engine.sslConnector
 import io.ktor.server.netty.Netty
 import io.ktor.server.routing.routing
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
@@ -141,6 +142,7 @@ internal object HTTPServer {
         return server.application.isActive
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     fun changePort(port: Int) {
         if (currServerPort == port && isServiceStarted) return
         GlobalScope.launch {
@@ -156,6 +158,7 @@ internal object HTTPServer {
         }
     }
     
+    @OptIn(DelicateCoroutinesApi::class)
     fun restart() {
         if(!isServiceStarted) return
         val post = currServerPort
